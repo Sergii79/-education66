7f63b3f66059d30f0eb2a404aa52d66154a20606
Sergii79 <tsymbal.sergiy@gmail.com>
2023-01-17 22:08:10 +0200

    1 commit

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,4 +1,4 @@
-console.log("Hello, world!")
+// console.log("Hello, world!")
 // Example 1 - Математичні оператори
 // Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
 
@@ -97,11 +97,126 @@ console.log("Hello, world!")
 
 //-----------------------------------------------------
 // Виносимо варіанти в масив
-const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
-const fruit = "cherry";
-// Перевіряємо присутність елемента
-const hasFruit = redFruits.includes(fruit);
+// const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
+// const fruit = "cherry";
+// // Перевіряємо присутність елемента
+// const hasFruit = redFruits.includes(fruit);
 
-if (hasFruit) {
-  console.log(`${fruit} is a red fruit!`);
-}
+// if (hasFruit) {
+//   console.log(`${fruit} is a red fruit!`);
+// }
+
+//-----------------------------------------------------
+// function foo(num) {
+//   console.log("foo:", num);
+//   // console.log(this);
+//   foo.count++;
+// }
+
+// foo.count = 0;
+
+// foo(5);
+// foo(3);
+// foo(19);
+// foo(5);
+
+// // console.dir(foo);
+// console.log(foo.count);
+
+//-----------------------------------------------------
+
+// function greetGuest(greeting) {
+//   console.log(`${greeting}, ${this.username}.`);
+// }
+
+// const mango = {
+//   username: "Манго",
+// };
+// const poly = {
+//   username: "Полі",
+// };
+
+// greetGuest.call(mango, "Ласкаво просимо"); // Ласкаво просимо, Манго.
+// greetGuest.call(poly, "З прибуттям"); // З прибуттям, Полі.
+
+//-----------------------------------------------------
+
+// "/this в callback-функціях/ Пояснить яким чином повязани  getFullName та callback()  /
+// const customer = {
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   getFullName() {
+//     console.log(this);
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() - це виклик методу getFullName без об'єкта
+//   console.log(`Обробляємо заявку від ${callback()}.`);
+// }
+
+// makeMessage(
+//   customer.getFullName.bind(customer)
+// ); // Буде помилка у виклику функції"
+
+//-----------------------------------------------------
+
+// function greet(clientName) {
+//   console.log( `${clientName}, ласкаво просимо в «${this.service}».`);
+  
+// }
+
+// const steam = {
+//   service: "Steam",
+// };
+// const steamGreeter = greet.bind(steam);
+// steamGreeter("Манго"); // "Манго, ласкаво просимо в «Steam»."
+
+
+// const gmail = {
+//   service: "Gmail",
+// };
+// const gmailGreeter = greet.bind(gmail);
+// gmailGreeter("Полі"); // "Полі, ласкаво просимо в «Gmail»."
+
+//-----------------------------------------------------
+
+// function foo() {
+//   console.log(this.a);
+// }
+
+// window.a = 5;
+
+// foo();
+
+// const obj = {
+//     a: 10,
+//     showA: foo
+// }
+
+// obj.showA()
+
+//-----------------------------------------------------
+
+//* Example 1 - Майстерня коштовностей
+// Напишіть метод calcTotalPrice(stoneName), який приймає назву каменю і розраховує та повертає загальну вартість каменів з таким ім'ям, ціною та кількістю з властивості stones.
+
+const chopShop = {
+  stones: [
+    { name: "Emerald", price: 1300, quantity: 4 },
+    { name: "Diamond", price: 2700, quantity: 3 },
+    { name: "Sapphire", price: 1400, quantity: 7 },
+    { name: "Ruby", price: 800, quantity: 2 },
+  ],
+  calcTotalPrice(stoneName) {
+    console.log(this)
+    // const currentStone = this.stones.find((stone) => stoneName === stone.name);
+    // return currentStone.price * currentStone.quantity;
+  },
+};
+
+console.log(chopShop.calcTotalPrice("Emerald")); // 5200
+console.log(chopShop.calcTotalPrice("Diamond")); // 8100
+console.log(chopShop.calcTotalPrice("Sapphire")); // 9800
+console.log(chopShop.calcTotalPrice("Ruby")); // 1600
