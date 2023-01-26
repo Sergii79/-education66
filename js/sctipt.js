19c8974df3ba5c1a3c1738a19e23cfa4ff9a93fe
Sergii79 <tsymbal.sergiy@gmail.com>
2023-01-26 14:26:56 +0200

    1 commit

--- js/sctipt.js
+++ js/sctipt.js
@@ -393,24 +393,25 @@
 
 //*-------------------------------------------------------
 
-class User {
-  constructor(email) {
-    this.email = email;
-  }
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
 
-  get email() {
-    return this.email;
-  }
+//   get email() {
+//     return this.email;
+//   }
 
-  set email(newEmail) {
-    this.email = newEmail;
-  }
-}
-// Change code below this line
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// // Change code below this line
 
-class Admin extends User {
-  static AccessLevel = {
-    BASIC: "basic", SUPERUSER: "superuser"
-  }  
-};
-console.log(Admin);
+// class Admin extends User {
+//   static AccessLevel = {
+//     BASIC: "basic", SUPERUSER: "superuser"
+//   }
+// };
+// console.log(Admin);
+
