058bb3e411d9ae865ba6e5140b141f2a4b80694a
Sergii79 <tsymbal.sergiy@gmail.com>
2023-01-27 23:01:15 +0200

    lesson 2-1

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,9 +1,42 @@
 // console.log("Hello, world!")
 //*-------------------------------------------------------
 
-const list = document.getElementById("list");
+// const list = document.getElementById("list");
 
-console.log(list);
+// // console.log(list);
 
+// if (list) {    
+//     console.log(list.children);
+// }
+// else {
+//     console.log("lthis element doesn't exist" );
+// }
 
+//*-------------------------------------------------------
 
+// - Урок 11
+//     1. Створити список технологій що вивчаються на курсі Fullstack за допомогою js
+        
+//         ```jsx
+        const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+//         ```
+//     Показати як че робиться через **map** та **reduce**
+
+/*
+    1. Створити контейнер для списку - тег ul
+    2. Створити цикл для перебoру елементів масиву
+    3. Створити тег li 
+    4. Записати текст з масиву у тег li
+    5. Помістити li всередину списку ul
+    6. Помістити список ul на сторінку
+*/
+
+const list = document.createElement("ul");
+
+for (const technology of technologies) {
+    const li = document.createElement("li");
+    console.log(technology);
+    li.textContent = technology;
+    list.append(li);
+}
+document.body.append(list);
