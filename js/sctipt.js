438ac08e44f006029528e98a53aa8cdee1c7ab49
Sergii79 <tsymbal.sergiy@gmail.com>
2023-01-30 21:27:41 +0200

    lesson 2-1

--- js/sctipt.js
+++ js/sctipt.js
@@ -18,9 +18,9 @@
 //     1. Створити список технологій що вивчаються на курсі Fullstack за допомогою js
         
 //         ```jsx
-        const conteyner = document.getElementById("conteyner");
+        // const conteyner = document.getElementById("conteyner");
         
-        const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+        // const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
 //         ```
 //     Показати як че робиться через **map** та **reduce**
 
@@ -33,7 +33,7 @@
     6. Помістити список ul на сторінку
 */
 
-const list = document.createElement("ul");
+// const list = document.createElement("ul");
 
 // for (const technology of technologies) {
 //     const li = document.createElement("li");
@@ -53,9 +53,69 @@ const list = document.createElement("ul");
 
 // console.log(listContent);
 
-const listContent = technologies
-    .reduce(
-        (acc, technology) => (acc += `<li>${technology}</li>`), ""
-);
-list.insertAdjacentHTML("beforeend", listContent);
-conteyner.append(list);
+//*--reduce1-------------
+
+// const listContent = technologies
+//     .reduce(
+//         (acc, technology) => (acc += `<li>${technology}</li>`), ""
+// );
+// list.insertAdjacentHTML("beforeend", listContent);
+// conteyner.append(list);
+
+//*--reduce2----------------
+
+// const listContent = technologies
+//     .reduce(
+//         (acc, technology) => (acc += `<li>${technology}</li>`), ""
+// );
+// const list = `<ul>${listContent}</ul>`
+
+// conteyner.insertAdjacentHTML("beforeend", list);
+
+// console.dir(document.body);
+
+//* 2. Створити кілька кнопок на основі масива з обєктами використовуючи createElement
+
+const container = document.getElementById("container");
+const colors = [
+  {
+    label: "red",
+    color: "#FF0000",
+  },
+  {
+    label: "green",
+    color: "#00FF00",
+  },
+  {
+    label: "blue",
+    color: "#0000FF",
+  },
+  {
+    label: "yellow",
+    color: "#FFFF00",
+  },
+];
+
+/*
+    1. Перебрати масив кольорів за допомогою циклу
+    2. Створити кнопку 
+    3. Задамо колір фону кнопки
+    4. Задамо текст кнопки
+    5. Додаємо кнопку в документ
+*/
+
+colors.forEach(({ label, color }) => {
+    const buttonEl = document.createElement("button");
+    buttonEl.style.backgroundColor = color;
+    buttonEl.textContent = label;
+    container.appendChild(buttonEl);
+});
+ 
+// colors.forEach(({ label, color }) => {
+//   const buttonEl = document.createElement("button");
+//   buttonEl.style.backgroundColor = color;
+//   buttonEl.classList.add("indents");
+//   //   buttonEl.style.margin = "5px"
+//   buttonEl.textContent = label;
+//   container.appendChild(buttonEl);
+// });
